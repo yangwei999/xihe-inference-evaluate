b4f5116a55d93dfc671902eb1cf4d4d169170fb6
zengchen1024 <chenzeng2@huawei.com>
2022-11-01 14:49:57 +0800

    fix run.sh again

--- infrastructure/inferenceimpl/image/run.sh
+++ infrastructure/inferenceimpl/image/run.sh
@@ -20,12 +20,12 @@ app=app.py
 
 # workspace
 dir=$(pwd)
-work_dir=$dir/{workspace}
+work_dir=$dir/workspace
 test -d $work_dir || mkdir $work_dir
 cd $work_dir
 
 # helper
-download_model {
+download_model() {
     local owner=$1
     local repo=$2
     local file=$repo/$3
@@ -37,29 +37,26 @@ download_model {
     if [ -n "$sha" ]; then
         # lfs file
 
-	$obs_util config -i=$obs_ak -k=$obs_sk -e=$obs_endpoint
+        $obs_util config -i=$obs_ak -k=$obs_sk -e=$obs_endpoint
 
-	# cp file
-	# ./obsutil cp obs://bucket-test/test1.txt  /test.txt
-	# sha[:2], sha[2:4], sha[4:]
-	sha=${sha#"oid sha256:"}
-	dst=$work_dir/$inference_dir/$(basename $file)
-	$obs_util cp obs://$obs_lfs_path/${sha:0:2}/${sha:2:2}/${sha:4} $dst
+        # cp file
+        # ./obsutil cp obs://bucket-test/test1.txt  /test.txt
+        # sha[:2], sha[2:4], sha[4:]
+        sha=${sha#"oid sha256:"}
+        dst=$work_dir/$inference_dir/$(basename $file)
+        $obs_util cp obs://$obs_lfs_path/${sha:0:2}/${sha:2:2}/${sha:4} $dst
 
-	if [ ! -e "$dst" ]; then
+        if [ ! -e "$dst" ]; then
             echo "no $dst"
             exit 1
         fi
     else
         # small file
-	mv $file $inference_dir
+        mv $file $inference_dir
     fi
-
-
-    # move model file to $inference_dir
 }
 
-# download project 
+# download project
 git clone $repo_url/$xihe_user/$project_name
 
 if [ ! -e "$inference_dir/$app" ]; then
@@ -70,19 +67,19 @@ fi
 f="./$inference_dir/config.json"
 
 if [ -e "$f" -a -s "$f" ]; then
-	v=$(python3 $dir/pretrain.py $f)
-	if [ $? -ne 0 ]; then
-		echo $v
-		exit 1
-	fi
+    v=$(python3 $dir/pretrain.py $f)
+    if [ $? -ne 0 ]; then
+        echo $v
+        exit 1
+    fi
 
-	if [ -n "$v" ]; then
-		owner=$(echo $v | sed -n '1p')
-		repo=$(echo $v | sed -n '2p')
-		pretain_file=$(echo $v | sed -n '3p')
+    if [ -n "$v" ]; then
+        owner=$(echo $v | sed -n '1p')
+        repo=$(echo $v | sed -n '2p')
+        pretain_file=$(echo $v | sed -n '3p')
 
-                download_model $owner $repo $pretain_file
-	fi
+        download_model $owner $repo $pretain_file
+    fi
 fi
 
 # run
