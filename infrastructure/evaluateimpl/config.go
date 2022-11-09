8fd3eb5f1b750c56dd72610e25160cbb46e6c144
zengchen1024 <chenzeng2@huawei.com>
2022-11-09 17:52:18 +0800

    fix load config

--- infrastructure/evaluateimpl/config.go
+++ infrastructure/evaluateimpl/config.go
@@ -5,8 +5,45 @@ import (
 	"github.com/opensourceways/xihe-inference-evaluate/infrastructure/inferenceimpl"
 )
 
+type configValidate interface {
+	Validate() error
+}
+
+type configSetDefault interface {
+	SetDefault()
+}
+
 type Config struct {
 	OBS inferenceimpl.OBSConfig `json:"-"`
+	CRD config.CRDConfig        `json:"crd" required:"true"`
+}
 
-	CRD config.CRDConfig `json:"crd" required:"true"`
+func (cfg *Config) configItems() []interface{} {
+	return []interface{}{
+		&cfg.CRD,
+	}
+}
+
+func (cfg *Config) SetDefault() {
+	items := cfg.configItems()
+
+	for _, i := range items {
+		if f, ok := i.(configSetDefault); ok {
+			f.SetDefault()
+		}
+	}
+}
+
+func (cfg *Config) Validate() error {
+	items := cfg.configItems()
+
+	for _, i := range items {
+		if f, ok := i.(configValidate); ok {
+			if err := f.Validate(); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
 }
--- infrastructure/inferenceimpl/config.go
+++ infrastructure/inferenceimpl/config.go
@@ -7,10 +7,49 @@ import (
 	"github.com/opensourceways/xihe-inference-evaluate/infrastructure/config"
 )
 
+type configValidate interface {
+	Validate() error
+}
+
+type configSetDefault interface {
+	SetDefault()
+}
+
 type Config struct {
 	OBS            OBSConfig        `json:"obs"              required:"true"`
 	CRD            config.CRDConfig `json:"crd"              required:"true"`
 	GitlabEndpoint string           `json:"gitlab_endpiont"  required:"true"`
+}
+
+func (cfg *Config) configItems() []interface{} {
+	return []interface{}{
+		&cfg.OBS,
+		&cfg.CRD,
+	}
+}
+
+func (cfg *Config) SetDefault() {
+	items := cfg.configItems()
+
+	for _, i := range items {
+		if f, ok := i.(configSetDefault); ok {
+			f.SetDefault()
+		}
+	}
+}
+
+func (cfg *Config) Validate() error {
+	items := cfg.configItems()
+
+	for _, i := range items {
+		if f, ok := i.(configValidate); ok {
+			if err := f.Validate(); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
 }
 
 type OBSConfig struct {
@@ -22,12 +61,12 @@ type OBSConfig struct {
 	Bucket      string `json:"bucket"        required:"true"`
 }
 
-func (c *Config) Validate() error {
-	if !filepath.IsAbs(c.OBS.OBSUtilPath) {
+func (c *OBSConfig) Validate() error {
+	if !filepath.IsAbs(c.OBSUtilPath) {
 		return errors.New("obsutil_path must be an absolute path")
 	}
 
-	if filepath.IsAbs(c.OBS.LFSPath) {
+	if filepath.IsAbs(c.LFSPath) {
 		return errors.New("lfs_path can't start with /")
 	}
 
