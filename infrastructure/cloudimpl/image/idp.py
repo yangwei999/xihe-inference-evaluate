7a436efa8ca6398639a4ecbcbc6d0c8e15037066
Li Chaoran <pkwarcraft@gmail.com>
2023-10-21 14:28:19 +0800

    fix header
    Signed-off-by: Li Chaoran <pkwarcraft@gmail.com>

--- infrastructure/cloudimpl/image/idp.py
+++ infrastructure/cloudimpl/image/idp.py
@@ -1,13 +1,10 @@
 import os
 
 import requests
-from typing import Dict, List
-from unittest import mock
 
-from jupyter_server.auth import Authorizer, IdentityProvider, User
-from tornado.httputil import url_concat
+from jupyter_server.auth import IdentityProvider, User
 from tornado.log import app_log
-from traitlets import Instance, default
+
 
 class XiheUser(User):
     """Subclass User to store JupyterHub data"""
@@ -24,28 +21,28 @@ class XiheIdentityProvider(IdentityProvider):
     """
 
     def get_user(self, handler):
-        ak = handler._headers.get("X-Auth-Request-Access-Token")
+        ak = handler.request.headers.get("X-Auth-Request-Access-Token")
         if ak is None:
-            print("missing access token")
+            app_log.error("missing access token")
             return None
 
         auth_url = os.getenv("ACCESS_TOKEN_ENDPOINT")
-        if auth_url:
-            print(f"missing auth url")
+        if auth_url is None:
+            app_log.error(f"missing auth url")
             return None
 
         res = requests.get(auth_url, headers={"Authorization": f"{ak}"})
         if res.status_code != 200:
-            print(f"get user failed: {res.status_code}")
+            app_log.error(f"get user failed: {res.status_code}")
             return None
 
         user = res.json().get("username")
         if user is None:
-            print(f"missing user data")
+            app_log.error(f"missing user data")
             return None
 
         env_name = os.getenv("USER")
-        print(f"{user} vs {env_name}")
+        app_log.info(f"{user} vs {env_name}")
         return XiheUser(env_name) if env_name == user else None
 
 c = get_config()  # noqa
